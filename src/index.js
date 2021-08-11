b35c39994528311768e8cc84182af0f6eb3ebf57
slawoe <info@slawomirernst.de>
2021-08-11 10:30:29 +0200

    Add middleware and thunk for asynchronysm

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnchancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
