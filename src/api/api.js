9c8117273ecbded58d0dd489f18990a837698852
slawoe <info@slawomirernst.de>
2021-08-11 10:32:16 +0200

    Update api.js so popularGamesURL is exported

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 
-//Base URL
 const base_url = `https://api.rawg.io/api/`;
 
-//Getting the date
 const getDate = (yearsDiff) => {
   let date = new Date().toISOString().slice(0, 10);
   if (yearsDiff != 0) {
@@ -13,13 +11,10 @@ const getDate = (yearsDiff) => {
   }
   return date;
 };
-
 const currentDate = getDate(0);
 const lastYear = getDate(-1);
 const nextYear = getDate(1);
 
-//Popular Games
 const popular_games = `games?key=${process.env.REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
-const popularGamesURL = () => `${base_url}${popular_games}`;
-console.log(popularGamesURL());
+export const popularGamesURL = () => `${base_url}${popular_games}`;
