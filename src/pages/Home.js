047eb3253fca7242209e154dfbde528bde09adc2
slawoe <info@slawomirernst.de>
2021-08-12 14:29:40 +0200

    Update Home.js for Game component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,46 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <GameList>
+      <h1>Upcoming Games</h1>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0;
+  h2 {
+    padding: 5rem 0;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
