00052cc4c4d3db07e201a5be01821ac9ace2a33c
slawoe <info@slawomirernst.de>
2021-08-13 11:59:46 +0200

    fix no image problem when searched

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,7 @@
 export const smallImage = (imagePath, size) => {
+  if (!imagePath) {
+    return null;
+  }
   const image = imagePath.match(/media\/screenshots/)
     ? imagePath.replace(
         "media/screenshots",
